20e16fb6bafc6411309b6db346bf4754ff3fc20d
Zenitramhgx <zenitramhgx15@hotmail.com>
2025-01-28 16:49:49 -0600

    Xura-15: fix: Sidebar, AuthContext y GraphBarAll

--- app/shared/layout/site/Sidebar.tsx
+++ app/shared/layout/site/Sidebar.tsx
@@ -1,25 +1,53 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, ReactElement } from 'react';
 import { Box, Typography } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
-// import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
-// import WysiwygOutlinedIcon from '@mui/icons-material/WysiwygOutlined';
-// import ContactMailOutlinedIcon from '@mui/icons-material/ContactMailOutlined';
-// import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
-// import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
+import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
+import WysiwygOutlinedIcon from '@mui/icons-material/WysiwygOutlined';
+import ContactMailOutlinedIcon from '@mui/icons-material/ContactMailOutlined';
+import GroupsOutlinedIcon from '@mui/icons-material/GroupsOutlined';
+import VpnKeyOutlinedIcon from '@mui/icons-material/VpnKeyOutlined';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { useRouter } from 'next/navigation';
+import AssessmentOutlinedIcon from '@mui/icons-material/AssessmentOutlined';
+import { useRouter, usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { useAuthContext } from '@/app/context/AuthContext';
 
-const menuItems = [
+type MenuItem = {
+  icon: ReactElement;
+  label: string;
+  link: string;
+  action?: 'logout';
+};
+
+const menuItems: MenuItem[] = [
   { icon: <HomeIcon />, label: 'Panel', link: '/panel' },
-  // { icon: <AppsOutlinedIcon />, label: 'Aplicaciones', link: '/aplicaciones' },
-  // { icon: <WysiwygOutlinedIcon />, label: 'Módulos', link: '/modulos' },
-  // { icon: <GroupsOutlinedIcon />, label: 'Grupos', link: '/grupos' },
-  // { icon: <VpnKeyOutlinedIcon />, label: 'Roles', link: '/roles' },
-  // { icon: <ContactMailOutlinedIcon />, label: 'Credenciales', link: '/credenciales' },
+  { icon: <AppsOutlinedIcon />, label: 'Aplicaciones', link: '/aplicaciones' },
+  { icon: <WysiwygOutlinedIcon />, label: 'Módulos', link: '/modulos' },
+  { icon: <GroupsOutlinedIcon />, label: 'Grupos', link: '/grupos' },
+  { icon: <VpnKeyOutlinedIcon />, label: 'Roles', link: '/roles' },
+  { icon: <ContactMailOutlinedIcon />, label: 'Credenciales', link: '/credenciales' },
+  {
+    icon: <LogoutIcon />,
+    label: 'Salir',
+    action: 'logout',
+    link: '/',
+  },
+];
+
+const menuItemsData: MenuItem[] = [
+  { icon: <HomeIcon />, label: 'Panel', link: '/panel' },
+  {
+    icon: <AssessmentOutlinedIcon />,
+    label: 'Captación',
+    link: '/data/captacion',
+  },
+  {
+    icon: <AssessmentOutlinedIcon />,
+    label: 'Matrícula',
+    link: '/data/matricula',
+  },
   {
     icon: <LogoutIcon />,
     label: 'Salir',
@@ -32,6 +60,7 @@ export default function Sidebar() {
   const [isOpen, setIsOpen] = useState(false);
   const { setUser } = useAuthContext();
   const router = useRouter();
+  const pathname = usePathname(); // Obtener la ruta actual
 
   const handleMouseEnter = () => setIsOpen(true);
   const handleMouseLeave = () => setIsOpen(false);
@@ -41,6 +70,8 @@ export default function Sidebar() {
     localStorage.removeItem('authToken');
     router.push('/');
   };
+
+  const currentMenuItems = pathname.startsWith('/data') ? menuItemsData : menuItems;
 
   return (
     <Box
@@ -61,47 +92,11 @@ export default function Sidebar() {
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      {menuItems.map((item) => (item.action === 'logout' ? (
-        <Box
-          key={item.label}
-          onClick={handleLogout}
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            width: '100%',
-            px: 2,
-            py: 2,
-            cursor: 'pointer',
-            transition: 'background-color 0.3s ease',
-            '&:hover': { bgcolor: '#e0e0e0' },
-          }}
-        >
+      {currentMenuItems.map((item) => (
+        item.action === 'logout' ? (
           <Box
-            sx={{
-              minWidth: 40,
-              color: '#444',
-              display: 'flex',
-              justifyContent: 'center',
-            }}
-          >
-            {item.icon}
-          </Box>
-          {isOpen && (
-            <Typography
-              sx={{
-                fontSize: '14px',
-                fontWeight: '500',
-                color: '#444',
-                ml: 2,
-              }}
-            >
-              {item.label}
-            </Typography>
-          )}
-        </Box>
-      ) : (
-        <Link href={item.link} key={item.label} style={{ textDecoration: 'none' }}>
-          <Box
+            key={item.label}
+            onClick={handleLogout}
             sx={{
               display: 'flex',
               alignItems: 'center',
@@ -136,8 +131,46 @@ export default function Sidebar() {
               </Typography>
             )}
           </Box>
-        </Link>
-      )))}
+        ) : (
+          <Link href={item.link} key={item.label} style={{ textDecoration: 'none' }}>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                width: '100%',
+                px: 2,
+                py: 2,
+                cursor: 'pointer',
+                transition: 'background-color 0.3s ease',
+                '&:hover': { bgcolor: '#e0e0e0' },
+              }}
+            >
+              <Box
+                sx={{
+                  minWidth: 40,
+                  color: '#444',
+                  display: 'flex',
+                  justifyContent: 'center',
+                }}
+              >
+                {item.icon}
+              </Box>
+              {isOpen && (
+                <Typography
+                  sx={{
+                    fontSize: '14px',
+                    fontWeight: '500',
+                    color: '#444',
+                    ml: 2,
+                  }}
+                >
+                  {item.label}
+                </Typography>
+              )}
+            </Box>
+          </Link>
+        )
+      ))}
     </Box>
   );
 }
